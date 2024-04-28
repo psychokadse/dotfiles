3059465bf74555c1363fe11f50ab70ea1a6d77e2
psychokadse <psyccvx42kits@proton.me>
2024-04-28 23:01:28 +0200

    Rewrote blurlock script, enable reacting to wallpaper changes

--- .config/i3/blurlock.bash
+++ .config/i3/blurlock.bash
@@ -1,22 +1,33 @@
 #!/bin/bash
 
-# take screenshot
+# Take screenshot
 # import -window root /tmp/screenshot.png
 
 nitrogen_config_home=~/.config/nitrogen
-blurred_path=/tmp/nitrogen-wallpaper-blur.png
+blurred_prefix=/tmp/nitrogen-wallpaper-blur_
 
+# Get path of currently selected nitrogen wallpaper
+wallpaper_path=$(awk -F ' *= *' '/file/ {print $2}' $nitrogen_config_home/bg-saved.cfg) 
+
+# Get only the file's basename
+wallpaper_filename=$(basename $wallpaper_path)
+
+# Concatenate to construct the blurred image's path
+blurred_path=$blurred_prefix$wallpaper_filename.png
+
+# Don't run if file already exists
 if [[ ! -f $blurred_path ]]
 then
-	wallpaper_path=$(awk -F ' *= *' '/file/ {print $2}' $nitrogen_config_home/bg-saved.cfg) 
-	
+	# Delete old files
+	rm $blurred_prefix*
+
 	convert $wallpaper_path -blur 0x5 $blurred_path
 fi
 
-# lock the screen
+# Lock the screen
 i3lock -i $blurred_path
 
-# sleep adds a small delay to prevent possible race conditions with suspend
+# Sleep adds a small delay to prevent possible race conditions with suspend
 sleep 0.25
 
 exit 0
