b2f4e849666763ccdbe1bbcfcb66290d1fd5fc20
psychokadse <psyccvx42kits@proton.me>
2024-04-28 21:11:10 +0200

    blurlock only blurs wallpaper, improved performance

--- .config/i3/blurlock.bash
+++ .config/i3/blurlock.bash
@@ -1,14 +1,20 @@
 #!/bin/bash
 
 # take screenshot
-import -window root /tmp/screenshot.png
+# import -window root /tmp/screenshot.png
 
-# blur it
-convert /tmp/screenshot.png -blur 0x5 /tmp/screenshotblur.png
-rm /tmp/screenshot.png
+nitrogen_config_home=~/.config/nitrogen
+blurred_path=/tmp/nitrogen-wallpaper-blur.png
+
+if [[ ! -f $blurred_path ]]
+then
+	wallpaper_path=$(awk -F ' *= *' '/file/ {print $2}' $nitrogen_config_home/bg-saved.cfg) 
+	
+	convert $wallpaper_path -blur 0x5 $blurred_path
+fi
 
 # lock the screen
-i3lock -i /tmp/screenshotblur.png
+i3lock -i $blurred_path
 
 # sleep adds a small delay to prevent possible race conditions with suspend
 sleep 0.25
