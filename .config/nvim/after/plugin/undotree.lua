f2b3605bd7d3063bfa3c270a4844ab4fda1fe62a
psychokadse <psyccvx42kits@proton.me>
2024-04-30 13:13:04 +0200

    Added undotree show binding

--- .config/nvim/after/plugin/undotree.lua
+++ .config/nvim/after/plugin/undotree.lua
@@ -1,2 +1,3 @@
 -- TODO Configure keymaps for undotree
-vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle)
+vim.keymap.set('n', '<leader>ut', vim.cmd.UndotreeToggle)
+vim.keymap.set('n', '<leader>us', vim.cmd.UndotreeShow)
