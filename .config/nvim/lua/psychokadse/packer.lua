ab139e0701ddbdd0c10ea0f21c6d76a4c4c3785e
psychokadse <psyccvx42kits@proton.me>
2024-05-28 21:24:53 +0200

    Removed commented out nvim plugin rose-pine

--- .config/nvim/lua/psychokadse/packer.lua
+++ .config/nvim/lua/psychokadse/packer.lua
@@ -10,16 +10,7 @@ return require('packer').startup(function(use)
 	  -- or                            , branch = '0.1.x',
 	  requires = { {'nvim-lua/plenary.nvim'} }
   }
-  --[[
-  use({
-	  'rose-pine/neovim',
-	  as = 'rose-pine',
-	  tag = 'v0.1.0', -- Optional tag release
-	  config = function()
-		  vim.cmd('colorscheme rose-pine')
-	  end
-  })
-  --]]
+
   use({
 	  'folke/tokyonight.nvim',
 	  as = 'tokyonight',
