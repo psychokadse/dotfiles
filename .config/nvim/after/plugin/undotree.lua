8d52059d213f8dbf97b0f8878cf4214eb30c4962
psychokadse <psyccvx42kits@proton.me>
2024-05-29 18:34:44 +0200

    Set undotree configs with better keybindings

--- .config/nvim/after/plugin/undotree.lua
+++ .config/nvim/after/plugin/undotree.lua
@@ -1,3 +1,5 @@
 -- TODO Configure keymaps for undotree
-vim.keymap.set('n', '<leader>ut', vim.cmd.UndotreeToggle)
-vim.keymap.set('n', '<leader>us', vim.cmd.UndotreeShow)
+vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle)
+
+vim.g.undotree_DiffAutoOpen = 1
+vim.g.undotree_SetFocusWhenToggle = 1
