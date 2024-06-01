fe05edc68c85ecdec4f9c96cecf25dd8cddd3630
psychokadse <psyccvx42kits@proton.me>
2024-06-01 17:50:04 +0200

    nvim: Mapped <leader>gs to toggle fugitive status

--- .config/nvim/after/plugin/fugitive.lua
+++ .config/nvim/after/plugin/fugitive.lua
@@ -1,1 +1,9 @@
-vim.keymap.set('n', '<leader>gs', vim.cmd.Git)
+vim.keymap.set('n', '<leader>gs', function()
+	for nr=1,vim.fn.winnr('$') do
+		if vim.fn.getwinvar(nr, 'fugitive_status') ~= '' then
+			vim.cmd.close({ count = nr })
+			return
+		end
+	end
+	vim.cmd.Git()
+end)
