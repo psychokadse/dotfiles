6c2a5800e34baaad69f51c4a207ccee65a6e5df4
psychokadse <psyccvx42kits@proton.me>
2024-06-06 19:44:23 +0200

    i3blocks: Added python script for memory block

---
+++ .config/i3blocks/memory.py
@@ -0,0 +1,29 @@
+#!/bin/python
+
+import os
+import subprocess
+
+mem_symbol = '\U000F035B'
+
+free_decoded = subprocess.run(['free', '-m'], capture_output=True) \
+    .stdout \
+    .decode() \
+    .splitlines()
+
+mem_stats_meg = free_decoded[1].split()
+
+mem_total_gig = float(mem_stats_meg[1]) / 1000
+mem_used_gig = float(mem_stats_meg[2]) / 1000
+mem_available_gig = float(
+    mem_stats_meg[len(mem_stats_meg) - 1]
+) / 1000
+
+message = f'{mem_symbol} {mem_used_gig:.2f} GiB/{mem_available_gig:.2f} GiB'
+one_percent = mem_total_gig / 100
+
+if mem_used_gig >= one_percent * 95:
+    print(f'<span foreground="{os.getenv('_color_urgent')}">{message}</span>')
+elif mem_used_gig >= one_percent * 90:
+    print(f'<span foreground="{os.getenv('_color_warn')}">{message}</span>')
+else:
+    print(message)
