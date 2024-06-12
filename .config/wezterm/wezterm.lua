84b1a2ce0eafb59ddbb7555b84012acdcfb690ff
psychokadse <psyccvx42kits@proton.me>
2024-06-12 15:17:40 +0200

    Set JetBrainsMono Nerd Font size 11 for dunst, i3 and wezterm

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,8 +1,8 @@
 local wezterm = require 'wezterm'
 local config = {}
 
-config.font = wezterm.font 'JetBrains Mono'
-config.font_size = 12.0
+config.font = wezterm.font 'JetBrainsMono Nerd Font'
+config.font_size = 11.0
 config.hide_tab_bar_if_only_one_tab = true
 config.window_background_opacity = .75
 
