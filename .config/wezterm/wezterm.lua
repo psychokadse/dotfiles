9e564c842fb187ec569da73bb7405dff3d8e9990
psychokadse <psyccvx42kits@proton.me>
2024-06-13 10:21:33 +0200

    wezterm: Disabled default keybindings, set own

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,9 +1,68 @@
 local wezterm = require 'wezterm'
+local act = wezterm.action
 local config = {}
 
+config.disable_default_key_bindings = true
 config.font = wezterm.font 'JetBrainsMono Nerd Font'
 config.font_size = 11.0
 config.hide_tab_bar_if_only_one_tab = true
+config.keys = {
+	{
+		key = '|',
+		mods = 'CTRL|ALT',
+		action = act.SplitVertical { domain = 'CurrentPaneDomain' }
+	},
+	{
+		key = '-',
+		mods = 'CTRL|ALT',
+		action = act.SplitHorizontal { domain = 'CurrentPaneDomain' }
+	},
+	{
+		key = 'h',
+		mods = 'CTRL|ALT',
+		action = act.ActivatePaneDirection('Left')
+	},
+	{
+		key = 'j',
+		mods = 'CTRL|ALT',
+		action = act.ActivatePaneDirection('Down')
+	},
+	{
+		key = 'k',
+		mods = 'CTRL|ALT',
+		action = act.ActivatePaneDirection('Up')
+	},
+	{
+		key = 'l',
+		mods = 'CTRL|ALT',
+		action = act.ActivatePaneDirection('Right')
+	},
+	{
+		key = '-',
+		mods = 'CTRL',
+		action = act.DecreaseFontSize
+	},
+	{
+		key = '+',
+		mods = 'CTRL|SHIFT',
+		action = act.IncreaseFontSize
+	},
+	{
+		key = '0',
+		mods = 'CTRL',
+		action = act.ResetFontSize
+	},
+	{
+		key = 'c',
+		mods = 'CTRL|SHIFT',
+		action = act.CopyTo('Clipboard')
+	},
+	{
+		key = 'v',
+		mods = 'CTRL|SHIFT',
+		action = act.PasteFrom('Clipboard')
+	}
+}
 config.window_background_opacity = .75
 
 return config
