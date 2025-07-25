e12736ede409c0459a854be80e16f676d74ff9d0
psychokadse <psyccvx42kits@proton.me>
2025-07-25 18:22:39 +0200

    feat(nvim): set better colors for line numbers
    - Use "moon" theme for `tokyonight`
    - Highlight line numbers using a lighter gray for better visibility
    - Highlight current line number using orange to find it more easily
    - Set color scheme after setting up `tokyonight` to ensure settings are
      applied correctly

--- src/local/.config/nvim/lua/psychokadse/colors.lua
+++ src/local/.config/nvim/lua/psychokadse/colors.lua
@@ -1,6 +1,5 @@
 function ReconfigureColors(color)
     color = color or "tokyonight"
-    vim.cmd.colorscheme(color)
 
     vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
     vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
@@ -8,10 +7,17 @@ function ReconfigureColors(color)
     vim.api.nvim_set_hl(0, "NormalSB", { bg = "none" })
 
     require("tokyonight").setup({
-        style = "storm",
+        style = "moon",
         transparent = true,
         terminal_colors = true,
+        on_highlights = function(hl, colors)
+            hl.LineNr = { fg = colors.orange }
+            hl.LineNrAbove = { fg = colors.dark5 }
+            hl.LineNrBelow = { fg = colors.dark5 }
+        end,
     })
+
+    vim.cmd.colorscheme(color)
 end
 
 ReconfigureColors()
