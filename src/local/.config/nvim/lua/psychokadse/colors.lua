7ffa82ef2d4a11512471dca4476b9396f08b4789
psychokadse <psyccvx42kits@proton.me>
2025-07-26 10:53:45 +0200

    feat(nvim): make background of floating windows transparent
    - Set `style.floats` to `"transparent"` in `tokyonight` setup
    - Remove calls to `vim.api.nvim_set_hl` as they are no longer necessary,
      as well as being too low-level of an interface for setting background
      styles when the theme offers easier and more reliable configuration
    
    This ensures all floating windows have a transparent background, without
    having to explicitly set the style of highlight groups.

--- src/local/.config/nvim/lua/psychokadse/colors.lua
+++ src/local/.config/nvim/lua/psychokadse/colors.lua
@@ -1,15 +1,13 @@
 function ReconfigureColors(color)
     color = color or "tokyonight"
 
-    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-    vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
-    vim.api.nvim_set_hl(0, "NormalSB", { bg = "none" })
-
     require("tokyonight").setup({
         style = "moon",
         transparent = true,
         terminal_colors = true,
+        styles = {
+            floats = "transparent",
+        },
         on_highlights = function(hl, colors)
             hl.LineNr = { fg = colors.orange }
             hl.LineNrAbove = { fg = colors.dark5 }
