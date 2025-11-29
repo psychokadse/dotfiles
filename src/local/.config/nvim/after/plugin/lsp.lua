57336220ae413763131787b372a7cd3ec006454c
psychokadse <psyccvx42kits@proton.me>
2025-11-29 22:08:59 +0100

    feat(nvim): add keybindings for scrolling docs
    Add the keybindings `<C-d>` and `<C-u>` for `nvim-cmp` in order to
    scroll the documentation window down and up respectively.

--- src/local/.config/nvim/after/plugin/lsp.lua
+++ src/local/.config/nvim/after/plugin/lsp.lua
@@ -56,6 +56,8 @@ local cmp_mappings = cmp.mapping.preset.insert({
     ["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
     ["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
     ["<C-y>"] = cmp.mapping.confirm({ select = true }),
+    ["<C-d>"] = cmp.mapping.scroll_docs(4),
+    ["<C-u>"] = cmp.mapping.scroll_docs(-4),
     ["<C-l>"] = cmp.mapping(function()
         if cmp.visible() then
             cmp.close()
