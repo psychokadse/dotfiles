ab3b315e6b5e8c4edfb03997c93d2f338b483fcb
psychokadse <psyccvx42kits@proton.me>
2026-04-30 03:51:52 +0200

    refactor(nvim): clean up lazy.nvim config
    - Remove unused plugins
    - Correct dependency relationships
    - Group plugins by domain

--- src/local/.config/nvim/lua/psychokadse/lazy.lua
+++ src/local/.config/nvim/lua/psychokadse/lazy.lua
@@ -12,58 +12,92 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
+    -- formatting
     {
         "stevearc/conform.nvim",
         config = require("psychokadse.plugin.format"),
     },
+
+    -- linting
+    {
+        "mfussenegger/nvim-lint",
+        -- config = require("psychokadse.plugin.lint"),
+    },
+
+    -- LSP tooling
+    {
+        "williamboman/mason.nvim",
+        --[[
+        config = function()
+            require("mason").setup()
+        end,
+		--]]
+    },
+
+    {
+        "WhoIsSethDaniel/mason-tool-installer.nvim",
+        --[[
+        config = function()
+            require("mason-tool-installer").setup({
+                ensure_installed = {
+                    "bashls",
+                    "black",
+                    "isort",
+                    "mypy",
+                    "lua_ls",
+                    "stylua",
+                    "nixfmt",
+                    "texlab",
+                    "cspell",
+                },
+            })
+        end,
+		--]]
+    },
+
+    {
+        "williamboman/mason-lspconfig.nvim",
+    },
+
+    -- completion stack
+    {
+        "hrsh7th/nvim-cmp",
+        dependencies = {
+            "hrsh7th/cmp-nvim-lsp",
+            "L3MON4D3/LuaSnip",
+        },
+    },
+
+    -- telescope
     {
         "nvim-telescope/telescope.nvim",
         dependencies = { "nvim-lua/plenary.nvim" },
     },
+
+    -- UI / visuals
     {
         "folke/tokyonight.nvim",
         name = "tokyonight",
-        build = function()
-            require("psychokadse.colors")
-        end,
         config = function()
             require("psychokadse.colors")
         end,
     },
+
+    {
+        "nvim-lualine/lualine.nvim",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+    },
+
+    -- editor features
+    "tpope/vim-fugitive",
+    "mbbill/undotree",
+    "ThePrimeagen/harpoon",
+
+    -- treesitter
     {
         "nvim-treesitter/nvim-treesitter",
         branch = "master",
         build = vim.cmd.TSUpdate,
         config = require("psychokadse.plugin.treesitter"),
-    },
-    "mbbill/undotree",
-    "tpope/vim-fugitive",
-    "ThePrimeagen/harpoon",
-    {
-        "VonHeikemen/lsp-zero.nvim",
-        dependencies = {
-            "williamboman/mason.nvim",
-            "williamboman/mason-lspconfig.nvim",
-            "neovim/nvim-lspconfig",
-            "hrsh7th/nvim-cmp",
-            "hrsh7th/cmp-nvim-lsp",
-            "L3MON4D3/LuaSnip",
-        },
-    },
-    {
-        "nvim-lualine/lualine.nvim",
-        dependencies = { "nvim-tree/nvim-web-devicons" },
-    },
-    {
-        "mhartington/formatter.nvim",
-    },
-    {
-        "williamboman/mason.nvim",
-        dependencies = {
-            "WhoIsSethDaniel/mason-tool-installer.nvim",
-        },
-    },
-    {
-        "mfussenegger/nvim-lint",
     },
 })
