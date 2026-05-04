7b925b8e2603f6b7a1b2a640a2937b3b5dcf5c6c
psychokadse <psyccvx42kits@proton.me>
2026-05-05 00:43:57 +0200

    fix(nvim): telescope using different current directory
    Prevent telescope from using its own working directory by explicitly
    setting it:
    - for `find_files` and `grep_string`, use the current directory already
      set by the autocmd
    - for `git_files`, ensure the root is the Git repository's root before
      invoking it
    
    This prevents telescope from reading file paths relative to the current
    directory detected by telescope's internal logic and subsequently
    opening the buffer in the wrong location relative to it.

--- src/local/.config/nvim/lua/psychokadse/plugin/telescope.lua
+++ src/local/.config/nvim/lua/psychokadse/plugin/telescope.lua
@@ -1,12 +1,22 @@
 return function()
+    local project = require("psychokadse.core.project")
     local builtin = require("telescope.builtin")
 
     vim.keymap.set("n", "<leader>pf", function()
-        builtin.find_files({ hidden = true, no_ignore = true, no_ignore_parent = true })
+        builtin.find_files({ cwd = vim.uv.cwd(), hidden = true, no_ignore = true, no_ignore_parent = true })
     end, { desc = "Find files" })
-    vim.keymap.set("n", "<C-p>", builtin.git_files, { desc = "Git files" })
+    vim.keymap.set("n", "<C-p>", function()
+        -- Ensure root is git repo root so paths don't break
+        project.ensure_root(project.get_git_root())
+        builtin.git_files()
+    end, { desc = "Git files" })
     vim.keymap.set("n", "<leader>ps", function()
-        builtin.grep_string({ search = vim.fn.input("Grep > "), use_regex = true, additional_args = { "--hidden" } }) -- Additional args correspond to ripgrep CLI args
+        builtin.grep_string({
+            cwd = vim.uv.cwd(),
+            search = vim.fn.input("Grep > "),
+            use_regex = true,
+            additional_args = { "--hidden" },
+        }) -- Additional args correspond to ripgrep CLI args
     end, { desc = "Grep string" })
 
     local telescope = require("telescope")
