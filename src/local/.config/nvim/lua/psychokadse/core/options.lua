3b7f6de46dfcf387254497aca345ede87eeb8597
psychokadse <psyccvx42kits@proton.me>
2026-05-07 22:14:31 +0200

    refactor(nvim): reorder and comment options
    In `psychokadse/core/options.lua`, reorder options by topic and add some
    clarifying comments.

--- src/local/.config/nvim/lua/psychokadse/core/options.lua
+++ src/local/.config/nvim/lua/psychokadse/core/options.lua
@@ -1,37 +1,45 @@
+-- line numbers
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
+-- indenting defaults
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
-
-vim.opt.fileencoding = "utf-8"
-
 vim.opt.smartindent = true
 
+-- files
+vim.opt.fileencoding = "utf-8"
+vim.opt.isfname:append("@-@") -- include literal `@` in filename detection
+
+-- text wrapping
 vim.opt.wrap = false
 
+-- swapfile
 vim.opt.swapfile = false
+vim.opt.updatetime = 50
+
+-- file backups
 vim.opt.backup = false
+
+-- undo
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
 
-vim.opt.hlsearch = false
+-- search
+vim.opt.hlsearch = false -- persistent search highlighting is annoying
 vim.opt.incsearch = true
 
+-- ui
 vim.opt.termguicolors = true
-
 vim.opt.scrolloff = 8
 vim.opt.signcolumn = "yes"
-vim.opt.isfname:append("@-@")
+vim.opt.colorcolumn = "120" -- 120 should be enough as a soft limit for code
+vim.opt.showmode = false -- lualine already shows the mode
 
-vim.opt.mouse = ""
+-- mouse support
+vim.opt.mouse = "" -- completely disable mouse support
 
-vim.opt.updatetime = 50
-
-vim.opt.colorcolumn = "120"
-
-vim.opt.showmode = false
-
+-- folding
 vim.opt.foldlevelstart = 99 -- start with all folds open
